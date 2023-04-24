1b18b73acd4a5b0957fdc170892467f0be9e8f34
zyn10 <zainandcs@gmail.com>
2023-04-24 21:24:57 +0500

    Enum

---
+++ contracts/2-Programming_Fundamentals/enumm.sol
@@ -0,0 +1,15 @@
+//particular number integer ko name assign krtay hain jab ham
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract enammmm {
+
+    enum user{allowed,not_allowed,wait}
+    user public u1= user.wait;
+
+    function owner() public{
+        if(u1==user.wait){
+            u1=user.allowed;
+        }
+    }
+}
